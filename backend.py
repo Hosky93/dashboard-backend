5f921c7809f7ecdab77e415ab275b36c521915a8
Andrew Hoskin <hosky@Mac.mynet>
2026-04-08 22:24:28 +0100

    Add production domain to CORS

--- backend.py
+++ backend.py
@@ -830,6 +830,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://easy-dash.io",
+        "https://www.easy-dash.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
